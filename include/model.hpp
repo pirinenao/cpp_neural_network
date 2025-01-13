39fa196a4fd6b3d32952b0b947df738bfcf5df73
api <e2202869@edu.vamk.fi>
2025-01-13 15:19:59 +0200

    CLI flags for epochs, learning rate, parallel computing and help

--- include/model.hpp
+++ include/model.hpp
@@ -8,12 +8,12 @@
  * trains the model using the training dataset
  */
 void model_train(mnist::MNIST_dataset<std::vector, std::vector<float>, int> dataset, LAYER &layer,
-                 LAYER &output_layer, EVALUATION eval, int num_epochs, int num_neurons, int num_classes, float learning_rate);
+                 LAYER &output_layer, EVALUATION eval, int num_epochs, int num_neurons, int num_classes, float learning_rate, int parallel);
 
 /*
  * evaluates model by using the validation dataset
  */
 void model_evaluate(mnist::MNIST_dataset<std::vector, std::vector<float>, int> dataset, LAYER &layer,
-                    LAYER &output_layer, EVALUATION eval, int num_neurons, int num_classes);
+                    LAYER &output_layer, EVALUATION eval, int num_neurons, int num_classes, int parallel);
 
 #endif
